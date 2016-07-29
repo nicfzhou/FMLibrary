44d2a5d78efba8f61385ec838e217e5790b6a6d2
nicfzhou <nicfzhou@gmail.com>
2016-07-29 11:14:45 +0800

    update toast api

--- Pod/Classes/UITool/FZToast.h
+++ Pod/Classes/UITool/FZToast.h
@@ -13,4 +13,13 @@
 + (void) makeToast:(NSString *) toast;
 + (void) makeToast:(NSString *)toast duration:(NSTimeInterval) duration;
 
+
++ (instancetype)make:(NSString *)text;
+- (FZToast *)bottom;
+- (FZToast *)top;
+- (FZToast *)center;
+- (FZToast *(^)(CGFloat))yOffset;
+- (FZToast *(^)(NSTimeInterval))duration;
+- (void)toast;
+
 @end
--- Pod/Classes/UITool/FZToast.m
+++ Pod/Classes/UITool/FZToast.m
@@ -8,20 +8,75 @@
 
 #import "FZToast.h"
 
+typedef NS_ENUM(NSUInteger,FZToastPostion) {
+    Center = 0,
+    Bottom = 1,
+    Top = 2
+};
+
+
 static NSMutableArray *toastWindowArray;
 @implementation FZToast{
     NSString *_text;
-    NSTimeInterval *_duration;
+    NSTimeInterval _duration;
+    FZToastPostion _postion;
+    CGFloat _offset;
 }
 
 + (void)initialize{
     toastWindowArray = [NSMutableArray array];
 }
+
+#pragma mark convenient
+
 + (void) makeToast:(NSString *) toast{
     [self makeToast:toast duration:2];
 }
 + (void) makeToast:(NSString *)toast duration:(NSTimeInterval) duration{
-    
+    [[FZToast make:toast].duration(duration) toast];
+}
+
+
+#pragma mark instanct public
++ (instancetype)make:(NSString *)text{
+    FZToast *toast = [[FZToast alloc] init];
+    [toast setText:text];
+    return toast;
+}
+
+- (instancetype) init{
+    if (self = [super init]) {
+        _duration = 2;
+        _offset   = 0;
+        _postion  = Bottom;
+    }
+    return self;
+}
+- (FZToast *)bottom{
+    _postion = Bottom;
+    return self;
+}
+- (FZToast *)top{
+    _postion = Top;
+    return self;
+}
+- (FZToast *)center{
+    _postion = Center;
+    return self;
+}
+- (FZToast *(^)(CGFloat))yOffset{
+    return ^FZToast *(CGFloat offset){
+        _offset = offset;
+        return self;
+    };
+}
+- (FZToast *(^)(NSTimeInterval))duration{
+    return ^FZToast *(NSTimeInterval duration){
+        _duration = duration;
+        return self;
+    };
+}
+- (void)toast{
     dispatch_async(dispatch_get_main_queue(), ^{
         
         
@@ -29,7 +84,7 @@ static NSMutableArray *toastWindowArray;
                                NSForegroundColorAttributeName:[UIColor whiteColor]};
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
         label.translatesAutoresizingMaskIntoConstraints = NO;
-        label.text            = toast;
+        label.text            = _text;
         label.font            = attr[NSFontAttributeName];
         label.textColor       = attr[NSForegroundColorAttributeName];
         label.backgroundColor = [UIColor colorWithRed:0.21 green:0.23 blue:0.24 alpha:1];
@@ -46,10 +101,9 @@ static NSMutableArray *toastWindowArray;
         size.height += 10;
         label.frame = CGRectMake(0, 0, size.width, size.height);
         CGRect windowFrame = CGRectMake((screenRect.size.width-size.width)*.5,
-                                       (screenRect.size.height - size.height - 30),
-                                       size.width,
-                                       size.height);
-        
+                                        (screenRect.size.height - size.height - 30),
+                                        size.width,
+                                        size.height);
         
         UIWindow *window = [[UIWindow alloc] initWithFrame:windowFrame];
         window.windowLevel = UIWindowLevelAlert;
@@ -57,26 +111,50 @@ static NSMutableArray *toastWindowArray;
         window.hidden = NO;
         [window addSubview:label];
         [toastWindowArray addObject:window];
+        CGFloat centerY = 0.f;
+        switch (_postion) {
+            case Center: {
+                centerY = screenRect.size.height*.5 + _offset;
+                break;
+            }
+            case Bottom: {
+                centerY = screenRect.size.height - 44 - windowFrame.size.height*.5 + _offset;
+                break;
+            }
+            case Top: {
+                centerY = 64 + windowFrame.size.height*.5 + _offset;
+                break;
+            }
+        }
+        window.center = CGPointMake(screenRect.size.width*.5, centerY);
+        
         
         [UIView animateWithDuration:0.25 animations:^{
             label.layer.opacity = 1.;
         }];
         
         
-        [label addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)]];
+        [label addGestureRecognizer:[[UITapGestureRecognizer alloc]
+                                     initWithTarget:self action:@selector(handleTap:)]];
         label.userInteractionEnabled = YES;
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(duration * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(_duration * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self hideView:label];
         });
     });
 }
 
-+ (void) handleTap:(UITapGestureRecognizer*) gr{
+
+#pragma mark private
+- (void)setText:(NSString *)text{
+    _text = text;
+}
+
+- (void) handleTap:(UITapGestureRecognizer*) gr{
     [self hideView:gr.view];
 }
 
-+ (void) hideView:(UIView*) view{
+- (void) hideView:(UIView*) view{
     [UIView animateWithDuration:.25 animations:^{
         view.layer.opacity = .0;
     } completion:^(BOOL finish){
